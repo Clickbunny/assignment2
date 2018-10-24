27935906856e5ce42c319be22fa133609924ee0d
Balvinder Kumar <kbalvinder1995@gmail.com>
2018-10-24 19:11:26 +0530

    assignment3

---
+++ tup1.py
@@ -0,0 +1,5 @@
+tup=(1,15,90,19,33,44)
+l=list(tup)
+l.reverse()
+new_tuple=l
+print(new_tuple)
---
+++ tup2.py
@@ -0,0 +1,3 @@
+tup=(13,66,18,99,78,-18,-19,77,33)
+print("maximum number is",max(tup))
+print("minimum number is", min(tup))
